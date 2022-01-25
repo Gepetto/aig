9d0b7f6a2a5cc4ffc8c6e725c354aebc611fb3c9
Nahuel Villa <nvilla@laas.fr>
2022-01-25 16:20:07 +0100

    API structure done

--- include/preview_IK/postures.hpp
+++ include/preview_IK/postures.hpp
@@ -3,6 +3,7 @@
 
 #include<Eigen/Dense>
 #include "pinocchio/parsers/sample-models.hpp"
+#include "pinocchio/algorithm/frames.hpp"
 
 namespace pin = pinocchio;
 namespace IK_tools {
@@ -12,43 +13,127 @@ namespace IK_tools {
     typedef Eigen::Matrix<double, 3, 1> xyzVector;
     typedef Eigen::Matrix<double, 6, 1> legJoints;
     enum Side {LEFT, RIGHT};
-
-    class Homogeneous
-    {
-    public:
-        HomMatrix matrix;
-        Homogeneous();
-        Homogeneous(const HomMatrix mat);
-
-        RotMatrix rotation() const;
-        xyzVector translation() const;
-        void rotation(const RotMatrix rot);
-        void translation(const xyzVector tran);
-    };
-
-    class Horizon
-    {
-    public:
-        Horizon();
-    };
-
-    class Chassis
+    
+    class BipedIK
     {   
     public:
         xyzVector comFromWaist;
         pin::Model model;
-        Eigen::VectorXd q0;
-        Chassis();
-        legJoints solveLeg(const Homogeneous &com, 
-                            const Homogeneous &foot, 
+        Eigen::VectorXd q0, q;
+        Eigen::VectorXd v, a;
+        BipedIK();
+        legJoints solveLeg(const pin::SE3 &com, 
+                            const pin::SE3 &foot, 
                             const Side &side) const;
-        Eigen::VectorXd computePosture(const Homogeneous &com, const Homogeneous &leftFoot,
-                                        const Homogeneous &rightFoot, Eigen::VectorXd q) const;
-        void setGlobalOrientation(Homogeneous &com, const Homogeneous &leftFoot,
-                                   const Homogeneous &rightFoot);
+        Eigen::VectorXd computePosture(const pin::SE3 &com, const pin::SE3 &leftFoot,
+                                        const pin::SE3 &rightFoot, Eigen::VectorXd q);
+        void setRootOrientation(pin::SE3 &com, const pin::SE3 &leftFoot, const pin::SE3 &rightFoot);
+        void computeJointDerivatives(const Eigen::VectorXd &q1, 
+                                                const Eigen::VectorXd &q2, 
+                                                const Eigen::VectorXd &q3, const double dt);
     };
 
 
+    struct LegSettings
+    {
+    public:
+        Side side;
+        double femurLength, tibiaLenght;
+        xyzVector hipFromWaist, ankleFromFoot;
+    };
+    class LegIG
+    {
+    public:
+        LegSettings info;
+
+        LegIG();
+        LegIG(const LegSettings &configuration);
+        legJoints solve(const pin::SE3 &base, const pin::SE3 &endEffector);
+
+    };
+    
+
+    class ArmIG
+    {
+    public:
+        ArmIG();
+        Eigen::VectorXd solve(const pin::SE3 &base, const pin::SE3 &endEffector);
+    };
+
+    struct BipedSettings
+    {
+    public:
+        std::string leftHipJoint, leftKneeJoint, leftAnkleJoint, leftFootFrame,
+                    rightHipJoint, rightKneeJoint, rightAnkleJoint, rightFootFrame;
+
+        xyzVector comFromWaist;
+        pin::Model model;
+    };
+    class BipIK
+    {
+    public:
+        BipedSettings info;
+        LegIG leftLeg, rightLeg;
+        ArmIG leftArm, rightArm;
+
+        BipIK();
+        BipIK(const xyzVector &setComFromWaist);
+        BipIK(const BipedSettings &configuration);
+        void checkCompatibility();//TODO
+        void solve(const xyzVector &com,
+                   const pin::SE3 &leftFoot,
+                   const pin::SE3 &rightFoot,
+                   const Eigen::VectorXd &q0,
+                   Eigen::VectorXd &posture);
+
+        void solve(const xyzVector &com, 
+                   const RotMatrix &baseRotation, 
+                   const pin::SE3 &leftFoot, 
+                   const pin::SE3 &rightFoot,
+                   const Eigen::VectorXd &q0, 
+                   Eigen::VectorXd &posture);
+
+        void solve(const pin::SE3 &base,
+                   const pin::SE3 &leftFoot,
+                   const pin::SE3 &rightFoot,
+                   const Eigen::VectorXd &q0,
+                   Eigen::VectorXd &posture);
+
+        void solve(const std::array<xyzVector, 3> &coms, 
+                   const std::array<pin::SE3, 3> &leftFeet,
+                   const std::array<pin::SE3, 3> &rightFeet,
+                   const Eigen::VectorXd &q0, 
+                   const double &dt,
+                         Eigen::VectorXd &posture, 
+                         Eigen::VectorXd &velocity, 
+                         Eigen::VectorXd &acceleration);
+        
+        void solve(const std::array<xyzVector, 3> &coms, 
+                   const std::array<RotMatrix, 3> &baseRotations,
+                   const std::array<pin::SE3, 3> &leftFeet,
+                   const std::array<pin::SE3, 3> &rightFeet,
+                   const Eigen::VectorXd &q0, 
+                   const double &dt,
+                         Eigen::VectorXd &posture, 
+                         Eigen::VectorXd &velocity, 
+                         Eigen::VectorXd &acceleration);
+
+        void solve(const std::array<pin::SE3, 3> &bases,
+                   const std::array<pin::SE3, 3> &leftFeet,
+                   const std::array<pin::SE3, 3> &rightFeet,
+                   const Eigen::VectorXd &q0, 
+                   const double &dt,
+                         Eigen::VectorXd &posture, 
+                         Eigen::VectorXd &velocity,
+                         Eigen::VectorXd &acceleration); 
+
+        void configurateLegs();
+        pin::SE3 computeBase(const xyzVector &com,
+                         const pin::SE3 &leftFoot,
+                         const pin::SE3 &rightFoot);
+        pin::SE3 computeBase(const xyzVector &com, 
+                         const RotMatrix &baseRotation);
+    };
 }
 
 
--- src/checker.cpp
+++ src/checker.cpp
@@ -67,14 +67,17 @@ void check_eigen_norms(){
 
 }
 
-void check_Homogeneous(){
-    IK_tools::Homogeneous H;
+void check_SE3(){
+
+    pin::SE3 H;
+    H.setIdentity();
     std::cout << "\n\n" << H.rotation() << "\n\n" << std::endl;
     std::cout << "\n\n" << H.translation() << "\n\n" << std::endl;
 }
 
 void checkChasisConstructor(){
-    IK_tools::Chassis H;
+
+    IK_tools::BipedIK H;
     // expected H.comFromWaist for talos: -0.003163900014529,  0.001237384291204, -0.142588610107038
     std::cout << "\n\nH.comFromWaist : " << H.comFromWaist << "\n\n" << std::endl;
 
@@ -85,20 +88,17 @@ void checkChasisConstructor(){
 void checkLegComputation(){
 
     IK_tools::legJoints leg;
-    IK_tools::Chassis H;
-    IK_tools::Homogeneous com, foot;
+    IK_tools::BipedIK H;
+    pin::SE3 com, foot;
     pin::Data data(H.model);
-
     
     pin::forwardKinematics(H.model, data, H.q0);
     pin::updateFramePlacements(H.model, data);
     std::string LF_name("leg_left_sole_fix_joint");
     long unsigned int LF_id = H.model.getFrameId(LF_name);
 
-    pin::SE3 LF(data.oMf[LF_id]);
-    foot.translation(LF.translation());
-    foot.rotation(LF.rotation());
-    
+    foot = data.oMf[LF_id];
+    com.setIdentity();
     com.translation(IK_tools::xyzVector(H.q0.head(3) + H.comFromWaist));
 
     // with q0, com = [-0.003163900014529,  0.001237384291204,  0.876681389892962]
@@ -114,8 +114,7 @@ void checkLegComputation(){
 void checkPostureComputation(){
 
     IK_tools::legJoints leg;
-    IK_tools::Chassis H;
-    IK_tools::Homogeneous com, leftFoot, rightFoot;
+    IK_tools::BipedIK H;
     pin::Data data(H.model);
 
     pin::forwardKinematics(H.model, data, H.q0);
@@ -123,13 +122,10 @@ void checkPostureComputation(){
     std::string LF_name("leg_left_sole_fix_joint"), RF_name("leg_right_sole_fix_joint");
     long unsigned int LF_id = H.model.getFrameId(LF_name), RF_id = H.model.getFrameId(RF_name);
 
-    pin::SE3 LF(data.oMf[LF_id]), RF(data.oMf[RF_id]);
-    leftFoot.translation(LF.translation());
-    leftFoot.rotation(LF.rotation());
-    rightFoot.translation(RF.translation());
-    rightFoot.rotation(RF.rotation());
-    
+    pin::SE3 leftFoot(data.oMf[LF_id]), rightFoot(data.oMf[RF_id]);
+    pin::SE3 com;
     com.translation(IK_tools::xyzVector(H.q0.head(3) + H.comFromWaist));
+    H.setRootOrientation(com, leftFoot, rightFoot);
 
     // with q0, com = [-0.003163900014529,  0.001237384291204,  0.876681389892962]
     // left  foot = [-0.00884695,  0.0848172, -2.02296e-06]
@@ -138,15 +134,46 @@ void checkPostureComputation(){
     std::cout << "\n\nLeft : " << leftFoot.translation() << std::endl;
     std::cout << "\n\nRight : " << rightFoot.translation() << std::endl;
 
-    Eigen::VectorXd q_new = H.computePosture(com, leftFoot, rightFoot, H.q0);
+    H.q = H.computePosture(com, leftFoot, rightFoot, H.q0);
 
-    std::cout << "\n\n" << q_new << "\n\n" << std::endl;
+    std::cout << "\n\n" << H.q << "\n\n" << std::endl;
     // the biggest element in the difference H.q0 - q_new is smaller than 0.0018
-    std::cout << "\n\n" << H.q0 - q_new << "\n\n" << std::endl;
+    std::cout << "\n\n" << H.q0 - H.q << "\n\n" << std::endl;
 }
 
+void checkDerivativesComputation(){
+
+    IK_tools::legJoints leg;
+    IK_tools::BipedIK H;
+    pin::Data data(H.model);
+
+    pin::forwardKinematics(H.model, data, H.q0);
+    pin::updateFramePlacements(H.model, data);
+    std::string LF_name("leg_left_sole_fix_joint"), RF_name("leg_right_sole_fix_joint");
+    long unsigned int LF_id = H.model.getFrameId(LF_name), RF_id = H.model.getFrameId(RF_name);
+
+    pin::SE3 leftFoot(data.oMf[LF_id]), rightFoot(data.oMf[RF_id]);
+    pin::SE3 com1, com2, com3;
+    com1.translation(IK_tools::xyzVector(H.q0.head(3) + H.comFromWaist));
+    com2.translation(IK_tools::xyzVector(0, 0.01, -0.02)+com1.translation());
+    com3.translation(IK_tools::xyzVector(0, 0.01, -0.02)+com2.translation());
+    H.setRootOrientation(com1, leftFoot, rightFoot);
+    H.setRootOrientation(com2, leftFoot, rightFoot);
+    H.setRootOrientation(com3, leftFoot, rightFoot);
+
+    Eigen::VectorXd q1 = H.computePosture(com1, leftFoot, rightFoot, H.q0);
+    Eigen::VectorXd q2 = H.computePosture(com2, leftFoot, rightFoot, H.q0);
+    Eigen::VectorXd q3 = H.computePosture(com3, leftFoot, rightFoot, H.q0);
+
+    H.computeJointDerivatives(q1, q2, q3, 0.01);
+
+    std::cout << "\n\n" << H.q << "\n\n" << std::endl;
+    std::cout << "\n\n" << H.q0 - H.q << "\n\n" << std::endl;
+    std::cout << "\n\n" << H.v << "\n\n" << std::endl;
+    std::cout << "\n\n" << H.a << "\n\n" << std::endl;
+}
 
 int main(){
-    checkPostureComputation();
+    checkDerivativesComputation();
 }
 
