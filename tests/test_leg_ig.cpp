a90b9be75ba63e0d81d884cf97be5d42be53c020
MaximilienNaveau <maximilien.naveau@gmail.com>
2022-02-08 17:44:41 +0100

    unit-tests compiles

--- tests/test_leg_ig.cpp
+++ tests/test_leg_ig.cpp
@@ -1,13 +1,13 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/test/tools/floating_point_comparison.hpp>
 
-#include "pinocchio/parsers/urdf.hpp"
-#include "pinocchio/parsers/srdf.hpp"
-#include "pinocchio/algorithm/kinematics.hpp"
 #include "pinocchio/algorithm/frames.hpp"
 #include "pinocchio/algorithm/joint-configuration.hpp"
-
-#include "preview_ik/unittests/pyrene_settings.hpp"
+#include "pinocchio/algorithm/kinematics.hpp"
+#include "pinocchio/parsers/srdf.hpp"
+#include "pinocchio/parsers/urdf.hpp"
 #include "preview_ik/leg_ig.hpp"
+#include "preview_ik/unittests/pyrene_settings.hpp"
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
@@ -30,30 +30,59 @@ BOOST_AUTO_TEST_CASE(test_leg_ig_init_constructor) {
   BOOST_CHECK_EQUAL(leg_ig.get_settings(), settings);
 }
 
-void generate_references(pinocchio::SE3& base, pinocchio::SE3& lf, pinocchio::SE3& rf, Eigen::VectorXd& q,
-                         preview_ik::LegJoints& ll_q, preview_ik::LegJoints& rl_q, const bool& random) {
+enum Mode{
+  ZERO,
+  HALF_SITTING,
+  RANDOM
+};
+
+void generate_references(pinocchio::SE3& base, pinocchio::SE3& lf,
+                         pinocchio::SE3& rf, preview_ik::LegJoints& ll_q,
+                         preview_ik::LegJoints& rl_q, const Mode& mode) {
   // Get the model and data
   pinocchio::Model model;
-  pinocchio::urdf::buildModel(preview_ik::unittests::urdf_path, pinocchio::JointModelFreeFlyer(), model);
+  pinocchio::urdf::buildModel(preview_ik::unittests::urdf_path,
+                              pinocchio::JointModelFreeFlyer(), model);
   pinocchio::Data data = pinocchio::Data(model);
-  pinocchio::srdf::loadReferenceConfigurations(model, preview_ik::unittests::srdf_path, false);
+  pinocchio::srdf::loadReferenceConfigurations(
+      model, preview_ik::unittests::srdf_path, false);
 
   // Generate a robot configuration.
-  if (random) {
-    q = randomConfiguration(model);
-  } else {
-    q = model.referenceConfigurations["half_sitting"];
+  Eigen::VectorXd q;
+  switch (mode) {
+    case Mode::ZERO:
+      q = Eigen::VectorXd::Zero(model.nq);
+      q(6) = 1.0;
+      break;
+    case Mode::HALF_SITTING:
+      q = model.referenceConfigurations["half_sitting"];
+      break;
+    case Mode::RANDOM:
+      model.lowerPositionLimit.head<3>().fill(-0.1);
+      model.upperPositionLimit.head<3>().fill(0.1);
+      q = randomConfiguration(model);
+      break;
+    default:
+      throw std::runtime_error(
+          "tes_leg_ig: generate_references():Switch default ask, not "
+          "implemented.");
+      break;
   }
 
   // Forward Kinematics.
   pinocchio::forwardKinematics(model, data, q);
   pinocchio::updateFramePlacements(model, data);
-  pinocchio::FrameIndex lf_id = model.getFrameId(preview_ik::unittests::left_foot_frame_name);
-  pinocchio::FrameIndex rf_id = model.getFrameId(preview_ik::unittests::right_foot_frame_name);
+  pinocchio::FrameIndex lf_id =
+      model.getFrameId(preview_ik::unittests::left_foot_frame_name);
+  pinocchio::FrameIndex rf_id =
+      model.getFrameId(preview_ik::unittests::right_foot_frame_name);
 
   // Get the legs joints configuration for the test
-  int lleg_idx_qs = model.idx_qs[model.getJointId(preview_ik::unittests::left_hip_joint_name)];
-  int rleg_idx_qs = model.idx_qs[model.getJointId(preview_ik::unittests::right_hip_joint_name)];
+  int lleg_idx_qs =
+      model
+          .idx_qs[model.getJointId(preview_ik::unittests::left_hip_joint_name)];
+  int rleg_idx_qs = model.idx_qs[model.getJointId(
+      preview_ik::unittests::right_hip_joint_name)];
 
   // outputs
   lf = data.oMf[lf_id];
@@ -63,6 +92,33 @@ void generate_references(pinocchio::SE3& base, pinocchio::SE3& lf, pinocchio::SE
   base = pinocchio::SE3(quat.toRotationMatrix(), q.head<3>());
   ll_q = q.segment<6>(lleg_idx_qs);
   rl_q = q.segment<6>(rleg_idx_qs);
+
+  std::cout << "q = " << q.transpose() << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(test_leg_ig_solve_left_zero) {
+  // create the solver
+  preview_ik::LegIGSettings settings;
+  settings.side = preview_ik::LegIGSettings::Side::LEFT;
+  settings.femur_length = 0.38;
+  settings.tibia_length = 0.325;
+  settings.hip_from_waist << -0.02, 0.085, -0.27105;
+  settings.ankle_from_foot << -0, -0, 0.107;
+  preview_ik::LegIG leg_ig(settings);
+
+  // perform a forward kinematics on a configuration
+  Eigen::VectorXd q;
+  preview_ik::LegJoints test_ll_q, test_rl_q;
+  pinocchio::SE3 base, lf, rf;
+  generate_references(base, lf, rf, test_ll_q, test_rl_q, Mode::ZERO);
+
+  // Compute inverse geometry.
+  preview_ik::LegJoints ll_q = leg_ig.solve(base, lf);
+
+  // Tests.
+  std::cout << "test_ll_q = " << test_ll_q.transpose() << std::endl;
+  std::cout << "ll_q = " << ll_q.transpose() << std::endl;
+  BOOST_CHECK(test_ll_q.isApprox(ll_q, 1e-5));
 }
 
 BOOST_AUTO_TEST_CASE(test_leg_ig_solve_left_half_sitting) {
@@ -77,17 +133,42 @@ BOOST_AUTO_TEST_CASE(test_leg_ig_solve_left_half_sitting) {
 
   // perform a forward kinematics on a configuration
   Eigen::VectorXd q;
+  preview_ik::LegJoints test_ll_q, test_rl_q;
   pinocchio::SE3 base, lf, rf;
-  generate_references(base, lf, rf, ll_q, rl_q, false);
+  generate_references(base, lf, rf, test_ll_q, test_rl_q, Mode::HALF_SITTING);
 
   // Compute inverse geometry.
-  preview_ik::LegJoints leg_joints = leg_ig.solve(base, lf);
+  preview_ik::LegJoints ll_q = leg_ig.solve(base, lf);
 
-  // get what to compare
-  Eigen::VectorXd test_leg_joints = q.segment<6>(lleg_idx_qs);
+  // Tests.
+  std::cout << "test_ll_q = " << test_ll_q.transpose() << std::endl;
+  std::cout << "ll_q = " << ll_q.transpose() << std::endl;
+  BOOST_CHECK(test_ll_q.isApprox(ll_q, 1e-5));
+}
 
-  // Tests
-  BOOST_CHECK_EQUAL(leg_joints, test_leg_joints);
+BOOST_AUTO_TEST_CASE(test_leg_ig_solve_left_random) {
+  // create the solver
+  preview_ik::LegIGSettings settings;
+  settings.side = preview_ik::LegIGSettings::Side::LEFT;
+  settings.femur_length = 0.38;
+  settings.tibia_length = 0.325;
+  settings.hip_from_waist << -0.02, 0.085, -0.27105;
+  settings.ankle_from_foot << -0, -0, 0.107;
+  preview_ik::LegIG leg_ig(settings);
+
+  // perform a forward kinematics on a configuration
+  Eigen::VectorXd q;
+  preview_ik::LegJoints test_ll_q, test_rl_q;
+  pinocchio::SE3 base, lf, rf;
+  generate_references(base, lf, rf, test_ll_q, test_rl_q, Mode::RANDOM);
+
+  // Compute inverse geometry.
+  preview_ik::LegJoints ll_q = leg_ig.solve(base, lf);
+
+  // Tests.
+  std::cout << "test_ll_q = " << test_ll_q.transpose() << std::endl;
+  std::cout << "ll_q = " << ll_q.transpose() << std::endl;
+  BOOST_CHECK(test_ll_q.isApprox(ll_q, 1e-5));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
