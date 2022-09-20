542e9a09a0b4361570fc0aabaa396b80ec01760c
Nahuel Villa <nvilla@laas.fr>
2022-09-20 20:40:32 +0200

    Force distribution in progress. Many things to do yet.

--- include/aig/c_dynamics.hpp
+++ include/aig/c_dynamics.hpp
@@ -17,53 +17,85 @@
 #include "pinocchio/multibody/model.hpp"
 #include "pinocchio/spatial/se3.hpp"
 #include <map>
+#include <memory>
 // clang-format on
 
 namespace dyno {
 
 enum contact { surface, point };
 
-struct ContactSettings {
+struct Contact6DSettings {
  public:
-  contact type;
   double mu, gu;
   double half_length, half_width;
+  bool active;
+  Eigen::Matrix<double, 6, 1> weights;
   std::string frame_name;
 
   friend std::ostream &operator<<(std::ostream &out,
-                                  const ContactSettings &obj) {
-    std::string type;
-    if (obj.type == contact::surface)
-      type = "surface";
-    else if (obj.type == contact::point)
-      type = "point";
-
-    out << "Contact " << type << ":\n";
+                                  const Contact6DSettings &obj) {
+    
+    out << "Contact6D " << ":\n";
+    out << "    mu: " << obj.active << "\n";
     out << "    mu: " << obj.mu << "\n";
     out << "    gu: " << obj.gu << "\n";
-    if (obj.type == contact::point)
-      out << std::endl;
-    else if (obj.type == contact::surface) {
-      out << "    Surface half_length: " << obj.half_length << "\n";
-      out << "    Surface half_width: " << obj.half_width << std::endl;
-    }
+    out << "    weights: " << obj.weights << "\n";
+    out << "    Surface half_length: " << obj.half_length << "\n";
+    out << "    Surface half_width: " << obj.half_width << std::endl;
+
     return out;
   }
 
-  friend bool operator==(const ContactSettings &lhs,
-                         const ContactSettings &rhs) {
+  friend bool operator==(const Contact6DSettings &lhs,
+                         const Contact6DSettings &rhs) {
     bool test = true;
-    test &= lhs.type == rhs.type;
     test &= lhs.frame_name == rhs.frame_name;
     test &= lhs.mu == rhs.mu;
+    test &= lhs.active == rhs.active;
     test &= lhs.gu == rhs.gu;
-
-    if (lhs.type == contact::point) return test;
-
     test &= lhs.half_length == rhs.half_length;
     test &= lhs.half_width == rhs.half_width;
+    test &= lhs.weights == rhs.weights;
     return test;
   }
+};
+
+class Contact6D{
+  private:
+    Contact6DSettings settings_;
+
+  public:
+    //matrices
+    Eigen::Matrix<double, 5, 6> unilaterality_A_;
+    Eigen::Matrix<double, 5, 1> unilaterality_b_;
+    Eigen::Matrix<double, 6, 6> friction_A_;
+    Eigen::Matrix<double, 6, 1> friction_b_;
+    Eigen::DiagonalMatrix<double, 6> regularization_A_;
+    Eigen::Matrix<double, 6, 1> regularization_b_;
+    Eigen::Matrix<double, 6, 6> newton_euler_A_;
+
+  
+    Contact6D();
+    Contact6D(const Contact6DSettings &settings);
+    void initialize(const Contact6DSettings &settings);
+
+    ~Contact6D();
+    
+    //setters
+    void active(const bool &active);
+    void setMu(const double &mu);
+    void setGu(const double &gu);
+    void setForceWeights(const Eigen::Vector3d &force_weights);
+    void setTorqueWeights(const Eigen::Vector3d &torque_weights);
+    void setSurfaceHalfWidth(const double &half_width);
+    void setSurfaceHalfLength(const double &half_length);
+    void updateNewtonEuler(const Eigen::Vector3d &CoM, 
+                           const pinocchio::SE3 &oMf);   //Check oMf, maybe we actually need fMo.
+
+    Eigen::Matrix<double, 6, 1> contactForce;
+
+    //getters
+    const Contact6DSettings &getSettings(){return settings_; }
 };
 
 struct DynoSettings {
@@ -93,9 +125,25 @@ class Dyno {
   Eigen::Matrix2d S_;
   pinocchio::Model model_;
   pinocchio::Data data_;
-  std::map<std::string, ContactSettings> contacts_;
   double mass_;
 
+  // Lists of contacts
+  // std::vector< std::shared_ptr<Contact6D> > known_contact6ds_;
+  std::map< std::string, std::shared_ptr< Contact6D > > known_contact6ds_;
+  std::vector< std::string > active_contact6ds_;
+
+  //QP Matrices
+  Eigen::MatrixXd unilaterality_A_;
+  Eigen::VectorXd unilaterality_b_;
+  Eigen::MatrixXd friction_A_;
+  Eigen::VectorXd friction_b_;
+  Eigen::DiagonalMatrix<double, -1> regularization_A_;
+  Eigen::VectorXd regularization_b_;
+  Eigen::Matrix<double, 6, -1> newton_euler_A_;
+  Eigen::Matrix<double, 6, 1> newton_euler_b_;
+
+  size_t uni_cols_, uni_rows_, fri_cols_, fri_rows_, reg_cols_, reg_rows_, ne_cols_;
+  
   // Internal variables:
   Eigen::Vector3d groundCoMForce_, groundCoMTorque_, nonCoPTorque_, weight_;
   Eigen::Vector3d acom_;
@@ -103,6 +151,11 @@ class Dyno {
   Eigen::Vector3d dL_;
   Eigen::Vector3d L_;
   Eigen::Vector2d n_;
+
+  void addSizes(const std::shared_ptr<Contact6D> &contact);
+  void removeSizes(const std::shared_ptr<Contact6D> &contact);
+  void resizeMatrices();
+  void buildMatrices();
 
  public:
   Dyno();
@@ -124,6 +177,17 @@ class Dyno {
                  bool flatHorizontalGround = true);
 
   void computeNL(const double &w);
+
+  void addContact6d(const std::shared_ptr<Contact6D> &contact,
+                    const std::string &name);
+  void removeContact6d(const std::string &name);
+
+  void activateContact6d(const std::string &name);
+  void deactivateContact6d(const std::string &name);
+
+  void distributeForce(const Eigen::Vector3d &groundCoMForce,
+                       const Eigen::Vector3d &groundCoMTorque,
+                       const Eigen::Vector3d &CoM);
 
   // GETTERS
   /// @brief Please call computeDynamics first.
--- src/c_dynamics.cpp
+++ src/c_dynamics.cpp
@@ -17,6 +17,86 @@
 
 namespace dyno {
 
+Contact6D::Contact6D() {}
+Contact6D::Contact6D(const Contact6DSettings &settings) {initialize(settings); }
+void Contact6D::initialize(const Contact6DSettings &settings) {
+
+  settings_ = settings;
+  double hl = settings_.half_length;
+  double hw = settings_.half_width;
+  double mu = settings_.mu;
+  double gu = settings_.gu;
+
+  //Make matrices
+  regularization_A_.diagonal() << settings_.weights;
+  regularization_b_ << Eigen::Matrix<double, 6, 1>::Zero();
+
+  unilaterality_A_ << Eigen::Matrix<double, 5, 6>::Zero();
+  unilaterality_A_.block<5, 1>(0, 2) << -1, -hl, -hw, -hl, -hw;
+  unilaterality_A_.block<2, 2>(1, 3) << 0, -1, 1, 0;
+  unilaterality_A_.block<2, 2>(3, 3) << 0, -1, 1, 0;
+  unilaterality_b_ << Eigen::Matrix<double, 6, 1>::Zero();
+
+  friction_A_ << Eigen::Matrix<double, 6, 6>::Zero();
+  friction_A_.block<6, 1>(0, 2) << -mu, -mu, -mu, -mu, -gu, -gu;
+  friction_A_.block<2, 2>(0, 0) << Eigen::Matrix2d::Identity();
+  friction_A_.block<2, 2>(2, 0) << -Eigen::Matrix2d::Identity();
+  friction_A_.block<2, 1>(4, 5) << 1, -1;
+  friction_b_ << Eigen::Matrix<double, 6, 1>::Zero();
+
+  newton_euler_A_ << Eigen::Matrix<double, 6, 6>::Zero();
+  newton_euler_A_.block<3, 3>(0, 0) << Eigen::Matrix3d::Identity();
+  newton_euler_A_.block<3, 3>(3, 3) << Eigen::Matrix3d::Identity();
+
+  //Note: newton_euler is not complete, it needs to be updated 
+  //with the com and contact position each time.
+
+  contactForce = Eigen::Matrix<double, 6, 1>::Zero();
+}
+
+void Contact6D::active(const bool &active){
+  settings_.active = active;
+  if (!active) contactForce.setZero();
+}
+
+void Contact6D::setForceWeights(const Eigen::Vector3d &force_weights){
+  settings_.weights.head<3>() = force_weights;
+  regularization_A_.diagonal().head<3>() = force_weights;
+}
+
+void Contact6D::setTorqueWeights(const Eigen::Vector3d &torque_weights){
+  settings_.weights.tail<3>() = torque_weights;
+  regularization_A_.diagonal().tail<3>() = torque_weights;
+}
+
+void Contact6D::setSurfaceHalfWidth(const double &half_width){
+  settings_.half_width = half_width;
+  unilaterality_A_(2, 2) = -half_width;
+  unilaterality_A_(4, 2) = -half_width;
+}
+
+void Contact6D::setSurfaceHalfLength(const double &half_length){
+  settings_.half_length = half_length;
+  unilaterality_A_(1, 2) = -half_length;
+  unilaterality_A_(3, 2) = -half_length;
+}
+
+void Contact6D::setMu(const double &mu){
+  settings_.mu = mu;
+  friction_A_.block<4, 1>(0, 2) << -mu, -mu, -mu, -mu;
+}
+
+void Contact6D::setGu(const double &gu){
+  settings_.gu = gu;
+  friction_A_.block<2, 1>(4, 2) << -gu, -gu;
+}
+
+void Contact6D::updateNewtonEuler(const Eigen::Vector3d &CoM, 
+                                  const pinocchio::SE3 &oMf){ 
+  newton_euler_A_.block<3, 3>(3, 0) << pinocchio::skew(oMf.translation() - CoM);
+}
+
+
 Dyno::Dyno() {}
 Dyno::Dyno(const DynoSettings settings) { initialize(settings); }
 
@@ -50,6 +130,16 @@ void Dyno::initialize(const DynoSettings settings) {
   }
   weight_ = mass_ * model_.gravity981;
   S_ << 0, -1, 1, 0;
+
+  // matrix_sizes
+  uni_cols_ = 0;
+  uni_rows_ = 0;
+  fri_cols_ = 0;
+  fri_rows_ = 0; 
+  reg_cols_ = 0;
+  reg_rows_ = 0;
+  ne_cols_ = 0;
+  newton_euler_b_.resize(6);
 }
 
 void Dyno::computeDynamics(const Eigen::VectorXd &posture,
@@ -100,4 +190,101 @@ void Dyno::computeNL(const double &w) {
    */
   n_ = acom_.head<2>() / (w * w) - data_.com[0].head<2>() + cop_;
 }
+
+// Contact management //////////////////////////////////////////////////////////////////
+
+void Dyno::addContact6d(const std::shared_ptr<Contact6D> &contact,
+                        const std::string &name){
+  
+  known_contact6ds_.insert(std::pair<std::string, 
+                           std::shared_ptr<Contact6D>>(name, contact));
+  
+  addSizes(known_contact6ds_[name]);
+  if (known_contact6ds_[name]->getSettings().active) activateContact6d(name);
+}
+
+void Dyno::removeContact6d(const std::string &name){
+  
+  removeSizes(known_contact6ds_[name]);
+  if (known_contact6ds_[name]->getSettings().active) deactivateContact6d(name);
+
+  known_contact6ds_.erase(name);
+}
+
+void Dyno::addSizes(const std::shared_ptr<Contact6D> &contact){
+  uni_rows_ += contact->unilaterality_A_.rows();
+  uni_cols_ += contact->unilaterality_A_.cols();
+  fri_rows_ += contact->friction_A_.rows(); 
+  fri_cols_ += contact->friction_A_.cols();
+  reg_rows_ += contact->regularization_A_.rows();
+  ne_cols_ += contact->newton_euler_A_.cols();
+
+  resizeMatrices();
+}
+
+void Dyno::removeSizes(const std::shared_ptr<Contact6D> &contact){ // @todo: These matrices are public to write this function, change it.
+  uni_rows_ -= contact->unilaterality_A_.rows();
+  uni_cols_ -= contact->unilaterality_A_.cols();
+  fri_rows_ -= contact->friction_A_.rows(); 
+  fri_cols_ -= contact->friction_A_.cols();
+  reg_rows_ -= contact->regularization_A_.rows();
+  ne_cols_ -= contact->newton_euler_A_.cols();
+
+  resizeMatrices();
+}
+
+void Dyno::resizeMatrices(){
+  unilaterality_A_.resize(uni_rows_, uni_cols_);
+  unilaterality_b_.resize(uni_rows_);
+  friction_A_.resize(fri_rows_, fri_cols_);
+  friction_b_.resize(fri_rows_);
+  regularization_A_.resize(reg_rows_);
+  regularization_b_.resize(reg_rows_);
+  newton_euler_A_.resize(6, ne_cols_);
+}
+
+void Dyno::activateContact6d(const std::string &name){
+  known_contact6ds_[name]->active(true);
+
+  active_contact6ds_.push_back(name);
+}
+
+void Dyno::deactivateContact6d(const std::string &name){
+  known_contact6ds_[name]->active(false);
+
+  for(auto it = active_contact6ds_.begin(); it != active_contact6ds_.end(); it++){
+    if (*it == name) active_contact6ds_.erase(it);
+  }
+}
+
+// const Eigen::Vector3d &groundCoMForce,
+// const Eigen::Vector3d &groundCoMTorque,
+// const Eigen::Vector3d &CoM
+void Dyno::buildMatrices(){
+
+  size_t uni_i=0, fri_i=0, reg_i=0, j=0, uni_r, fri_r, reg_r, cols;
+
+  for(std::string name : active_contact6ds_){
+    std::shared_ptr<Contact6D> &contact = known_contact6ds_[name];
+    //@Todo: update the newton_euler cross product
+
+    uni_r = contact->unilaterality_A_.rows();
+    fri_r = contact->friction_A_.rows();
+    reg_r = contact->regularization_A_.rows();
+    cols = contact->newton_euler_A_.cols();
+
+    unilaterality_A_.block(uni_i, j, uni_r, cols) << contact->unilaterality_A_;
+    unilaterality_b_.segment(uni_i, uni_r) << contact->unilaterality_b_;
+    friction_A_.block(fri_i, j, fri_r, cols) << contact->friction_A_;
+    friction_b_.segment(fri_i, fri_r) << contact->friction_b_;
+    regularization_A_.diagonal().segment(reg_i, reg_r) << contact->regularization_A_.diagonal(); // @TODO : use a vector instead, we can transform it in matrix at the last moment.
+    regularization_b_.segment(reg_i, reg_r) << contact->regularization_b_;
+    newton_euler_A_.block(0, j, 6, cols) << contact->newton_euler_A_;//@Todo: make and include here the adjoint matrix.
+    
+    uni_i += uni_r; fri_i += fri_r; reg_i += reg_r; j += cols;
+  }
+  newton_euler_b_ << groundCoMForce_, groundCoMTorque_;
+}
+
+
 }  // namespace dyno
