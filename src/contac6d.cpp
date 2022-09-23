1d15776bdcecc9d6b8a417ca7ecdb2cc3a3f6f88
Nahuel Villa <nvilla@laas.fr>
2022-09-23 18:51:08 +0200

    bindings working, code improved

---
+++ src/contac6d.cpp
@@ -0,0 +1,85 @@
+/**
+ * @file
+ * @copyright Copyright (c) 2022, LAAS-CNRS, Toward, PalRobotics
+ * @brief
+ */
+
+#include "aig/contact6d.hpp"
+#include "pinocchio/algorithm/geometry.hpp"
+
+namespace aig {
+
+Contact6D::Contact6D() {}
+Contact6D::Contact6D(const Contact6DSettings &settings) {
+  initialize(settings);
+}
+void Contact6D::initialize(const Contact6DSettings &settings) {
+  settings_ = settings;
+  double hl = settings_.half_length;
+  double hw = settings_.half_width;
+  double mu = settings_.mu;
+  double gu = settings_.gu;
+
+  // Make matrices
+  regularization_A_ << settings_.weights;
+  regularization_b_ << Eigen::Matrix<double, 6, 1>::Zero();
+
+  unilaterality_A_ << Eigen::Matrix<double, 5, 6>::Zero();
+  unilaterality_A_.block<5, 1>(0, 2) << -1, -hl, -hw, -hl, -hw;
+  unilaterality_A_.block<2, 2>(1, 3) << 0, -1, 1, 0;
+  unilaterality_A_.block<2, 2>(3, 3) << 0, 1, -1, 0;
+  unilaterality_b_ << Eigen::Matrix<double, 6, 1>::Zero();
+
+  friction_A_ << Eigen::Matrix<double, 6, 6>::Zero();
+  friction_A_.block<6, 1>(0, 2) << -mu, -mu, -mu, -mu, -gu, -gu;
+  friction_A_.block<2, 2>(0, 0) << Eigen::Matrix2d::Identity();
+  friction_A_.block<2, 2>(2, 0) << -Eigen::Matrix2d::Identity();
+  friction_A_.block<2, 1>(4, 5) << 1, -1;
+  friction_b_ << Eigen::Matrix<double, 6, 1>::Zero();
+
+  newton_euler_A_ << Eigen::Matrix<double, 6, 6>::Zero();
+  newton_euler_A_.block<3, 3>(0, 0) << Eigen::Matrix3d::Identity();
+  newton_euler_A_.block<3, 3>(3, 3) << Eigen::Matrix3d::Identity();
+
+  // Note: newton_euler must be updated before using it
+  contactForce_ = Eigen::Matrix<double, 6, 1>::Zero();
+}
+
+void Contact6D::setForceWeights(const Eigen::Vector3d &force_weights) {
+  settings_.weights.head<3>() = force_weights;
+  regularization_A_.head<3>() = force_weights;
+}
+
+void Contact6D::setTorqueWeights(const Eigen::Vector3d &torque_weights) {
+  settings_.weights.tail<3>() = torque_weights;
+  regularization_A_.tail<3>() = torque_weights;
+}
+
+void Contact6D::setSurfaceHalfWidth(const double &half_width) {
+  settings_.half_width = half_width;
+  unilaterality_A_(2, 2) = -half_width;
+  unilaterality_A_(4, 2) = -half_width;
+}
+
+void Contact6D::setSurfaceHalfLength(const double &half_length) {
+  settings_.half_length = half_length;
+  unilaterality_A_(1, 2) = -half_length;
+  unilaterality_A_(3, 2) = -half_length;
+}
+
+void Contact6D::setMu(const double &mu) {
+  settings_.mu = mu;
+  friction_A_.block<4, 1>(0, 2) << -mu, -mu, -mu, -mu;
+}
+
+void Contact6D::setGu(const double &gu) {
+  settings_.gu = gu;
+  friction_A_.block<2, 1>(4, 2) << -gu, -gu;
+}
+
+void Contact6D::updateNewtonEuler(const Eigen::Vector3d &CoM,
+                                  const pinocchio::SE3 &oMf) {
+  newton_euler_A_.block<3, 3>(3, 0) << pinocchio::skew(oMf.translation() - CoM);
+  oAf_ = oMf.toActionMatrixInverse().transpose();
+}
+}
--- tests/python/test_contacts.py
+++ tests/python/test_contacts.py
@@ -5,3 +5,37 @@ Created on Thu Sep 22 16:28:36 2022
 
 @author: nvilla
 """
+import aig
+import numpy as np
+
+settings = aig.Contact6DSettings()
+settings.frame_name = "leg_left_sole_fix_joint"
+settings.mu = 7
+settings.gu = 9
+settings.weights = np.array([1, 2, 3, 4, 5, 6])
+settings.half_length = 0.5
+settings.half_width = 0.3
+
+contact = aig.Contact6D()
+contact.initialize(settings)
+
+print("fri_A: \n", contact.fri_A())
+print("fri_b: \n", contact.fri_b())
+print("uni_A: \n", contact.uni_A())
+print("uni_b: \n", contact.uni_b())
+print("reg_A: \n", contact.reg_A())
+print("reg_b: \n", contact.reg_b())
+print("NE_A: \n", contact.NE_A())
+
+dynSettings = aig.DynaCoMSettings()
+dynSettings.urdf = "/local/users/nvilla/workspace/install/share/example-robot-data/robots/talos_data/robots/talos_reduced.urdf"
+
+d = aig.DynaCoM()
+d.initialize(dynSettings)
+d.addContact6d(contact, "U")
+
+d.removeContact6d("U")
+
+d.addContact6d(contact, "G")
+d.activateContact6d("G")
+d.deactivateContact6d("G")
