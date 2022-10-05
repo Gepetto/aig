b686c03d2e7fe0b3d7542022fb5654160073f542
Nahuel Villa <nvilla@laas.fr>
2022-10-05 20:48:09 +0200

    Force distributor, distributing

--- include/aig/dyna_com.hpp
+++ include/aig/dyna_com.hpp
@@ -21,6 +21,12 @@
 #include "aig/contact6d.hpp"
 // clang-format on
 
+// @TODO: Separate the DynaCom in a new repository.
+// @TODO: add abstract class contact and new implementation of it contactPoint. 
+// @TODO: implement method copy of contacts to be able to duplicate them easily.
+// @TODO: In the getters uni_A, fria_A, uni_b, fri_b return only the active block.
+// @TODO: change internaly the system of names by ids.
+
 namespace aig {
 
 struct DynaCoMSettings {
@@ -70,7 +76,7 @@ class DynaCoM {
   Eigen::Matrix<double, 6, 1> newton_euler_b_;
 
   Eigen::MatrixXd H_, C_, A_;
-  Eigen::VectorXd g_, u_;
+  Eigen::VectorXd g_, u_, l_;
   Eigen::Matrix<double, 6, 1> b_;
   Eigen::VectorXd F_;
   Eigen::VectorXi ActiveSet_;
@@ -150,6 +156,8 @@ class DynaCoM {
     return known_contact6ds_[name];
   }
   const DynaCoMSettings &getSettings() { return settings_; }
+  const pinocchio::Model &getModel() {return model_;}
+  const pinocchio::Data &getData() {return data_;}
 
   const Eigen::MatrixXd &uni_A() { return unilaterality_A_; }
   const Eigen::VectorXd &uni_b() { return unilaterality_b_; }
--- tests/python/test_contacts.py
+++ tests/python/test_contacts.py
@@ -8,42 +8,96 @@ Created on Thu Sep 22 16:28:36 2022
 import aig
 import numpy as np
 from example_robot_data.path import EXAMPLE_ROBOT_DATA_MODEL_DIR
+import unittest
+import pinocchio as pin
+
+unittest.util._MAX_LENGTH = 2000
 
 
-settings = aig.Contact6DSettings()
-settings.frame_name = "leg_left_sole_fix_joint"
-settings.mu = 1
-settings.gu = 1
-settings.weights = np.array([1, 1, 1, 1, 1, 1])
-settings.half_length = 0.00005
-settings.half_width = 0.00005
+class TestDynaCoM(unittest.TestCase):
+    def setUp(self):
 
-contact = aig.Contact6D()
-contact.initialize(settings)
+        ## CONTACTS ##
+        settingsL = aig.Contact6DSettings()
+        settingsL.frame_name = "leg_left_sole_fix_joint"
+        settingsL.mu = 0.3
+        settingsL.gu = 0.4
+        settingsL.weights = np.array([1, 1, 1, 1, 1, 1])
+        settingsL.half_length = 0.1
+        settingsL.half_width = 0.05
 
-print("fri_A: \n", contact.fri_A())
-print("fri_b: \n", contact.fri_b())
-print("uni_A: \n", contact.uni_A())
-print("uni_b: \n", contact.uni_b())
-print("reg_A: \n", contact.reg_A())
-print("reg_b: \n", contact.reg_b())
-print("NE_A: \n", contact.NE_A())
+        leftSole = aig.Contact6D()
+        leftSole.initialize(settingsL)
 
-dynSettings = aig.DynaCoMSettings()
-dynSettings.urdf = (
-    EXAMPLE_ROBOT_DATA_MODEL_DIR + "/talos_data/robots/talos_reduced.urdf"
-)
+        settingsR = aig.Contact6DSettings()
+        settingsR.frame_name = "leg_right_sole_fix_joint"
+        settingsR.mu = 0.3
+        settingsR.gu = 0.4
+        settingsR.weights = np.array([1, 1, 3, 1, 1, 1])
+        settingsR.half_length = 0.1
+        settingsR.half_width = 0.05
 
-d = aig.DynaCoM()
-d.initialize(dynSettings)
-d.addContact6d(contact, "U")
+        rightSole = aig.Contact6D()
+        rightSole.initialize(settingsR)
 
-# d.removeContact6d("U")
+        ### DYNAMICS ###
+        dynSettings = aig.DynaCoMSettings()
+        dynSettings.urdf = (
+            EXAMPLE_ROBOT_DATA_MODEL_DIR + "/talos_data/robots/talos_reduced.urdf"
+        )
 
-d.addContact6d(contact, "G")
-d.activateContact6d("G")
+        d = aig.DynaCoM()
+        d.initialize(dynSettings)
+
+        pin.loadReferenceConfigurations(
+            d.model(),
+            EXAMPLE_ROBOT_DATA_MODEL_DIR + "/talos_data/srdf/talos.srdf",
+            False,
+        )
+
+        q0 = d.model().referenceConfigurations["half_sitting"]
+        v0 = np.zeros(d.model().nv)
+        d.computeDynamics(q0, v0, v0, np.zeros(6), True)
+        pin.computeAllTerms(d.model(), d.data(), q0, v0)
+        pin.updateFramePlacements(d.model(), d.data())
+
+        d.addContact6d(leftSole, "left_sole")
+        d.addContact6d(rightSole, "right_sole")
+
+        self.dyn = d
+        self.setL = settingsL
+        self.setR = settingsR
+
+    def test_contacts(self):
+
+        leftSole = self.dyn.getContact("left_sole")
+
+        self.assertTrue(leftSole.get_settings()["mu"] == self.setL.mu)
+        leftSole.set_mu(0.6)
+        self.assertTrue(leftSole.get_settings()["mu"] == 0.6)
+
+        self.assertTrue((leftSole.get_settings()["weights"] == self.setL.weights).all())
+        leftSole.set_force_weights(np.array([3, 3, 3]))
+        self.assertTrue(
+            (leftSole.get_settings()["weights"][:3] == np.array([3, 3, 3])).all()
+        )
+        leftSole.set_torque_weights(np.array([3, 3, 3]))
+        self.assertTrue(
+            (leftSole.get_settings()["weights"][3:] == np.array([3, 3, 3])).all()
+        )
+
+    def test_distributor(self):
+
+        self.dyn.distributeForce(
+            np.array([0, 0, 0]), np.array([1, 0, 0]), self.dyn.data().com[0]
+        )
+        leftSole = self.dyn.getContact("left_sole")
+        rightSole = self.dyn.getContact("right_sole")
+
+        print(leftSole.appliedForce()[:])
+        print(rightSole.appliedForce()[:])
+        print(leftSole.appliedForce()[:] + rightSole.appliedForce()[:])
 
 
-d.deactivateContact6d("G")
-
-d.distributeForce(np.array([0, 0, 1]), np.array([0, 0, 0]), np.array([0, 0, 1]))
+if __name__ == "__main__":
+    unittest.main()
