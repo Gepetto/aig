55f0f4c94837a8b77630778acc5d52dee3d3dea2
Nahuel Villa <nvilla@laas.fr>
2022-10-25 18:33:38 +0200

    BipedIG tests

--- tests/python/test_biped_ig.py
+++ tests/python/test_biped_ig.py
@@ -12,10 +12,10 @@ from example_robot_data.path import EXAMPLE_ROBOT_DATA_MODEL_DIR
 
 import unittest
 import pinocchio as pin
-from time import sleep
+from time import sleep, time
 
 debug = False
-visualize = True
+visualize = False
 
 # unittest.util._MAX_LENGTH = 2000
 
@@ -70,30 +70,89 @@ class TestBipedIG(unittest.TestCase):
         self.Base_id = 1
 
     def test_base_postures(self):
-
-        LF = pin.SE3(np.eye(3), np.array([0.0, 0.28, 0]))
-        RF = pin.SE3(np.eye(3), np.array([0.0, -0.58, 0]))
-        BS = pin.SE3(np.eye(3), np.array([0, 0, 0.8]))
-
-        posture = self.biped.solve(BS, LF, RF, self.q0)
-
-        pin.forwardKinematics(self.model, self.data, posture)
-        pin.updateFramePlacements(self.model, self.data)
-
-        outLF = self.data.oMf[self.LF_id]
-        outRF = self.data.oMf[self.RF_id]
-        outBS = self.data.oMi[self.Base_id]
-
-        print(outLF, LF)
-        print(outRF, RF)
-        print(outBS, BS)
-
+        e = 1e-11
         if visualize:
             v = get_visualizer()
-            v.display(posture)
-            sleep(1)
+
+        L_pos = [
+            np.array([0.0, 0.28, 0]),
+            np.array([0.04, 0.05, 0]),
+            np.array([-0.05, 0, 0]),
+            np.array([0.1, 0.4, 0.3]),
+        ]
+        R_pos = [
+            np.array([0.0, -0.38, 0]),
+            np.array([-0.14, -0.15, 0]),
+            np.array([-0.52, 0, 0]),
+            np.array([-0.3, 0, 0.15]),
+        ]
+        BS = pin.SE3(np.eye(3), np.array([0, 0, 0.8]))
+
+        for L, R in zip(L_pos, R_pos):
+            LF = pin.SE3(np.eye(3), L)
+            RF = pin.SE3(np.eye(3), R)
+
+            posture = self.biped.solve(BS, LF, RF, self.q0)
+
+            if visualize:
+                v.display(posture)
+                sleep(1)
+
+            pin.forwardKinematics(self.model, self.data, posture)
+            pin.updateFramePlacements(self.model, self.data)
+
+            outLF = self.data.oMf[self.LF_id]
+            outRF = self.data.oMf[self.RF_id]
+            outBS = self.data.oMi[self.Base_id]
+
+            self.assertTrue((LF.homogeneous - outLF.homogeneous < e).all())
+            self.assertTrue((RF.homogeneous - outRF.homogeneous < e).all())
+            self.assertTrue((BS.homogeneous - outBS.homogeneous < e).all())
 
         # self.assertTrue(out)
+
+    def test_com_postures(self):
+        e = 1e-11
+        if visualize:
+            v = get_visualizer()
+
+        LF = pin.SE3(np.eye(3), np.array([0.15, -0.18, 0.2]))
+        RF = pin.SE3(np.eye(3), np.array([0.3, -0.38, 0.2]))
+        CoM = np.array([0, 0, 0.8])
+
+        for i in range(20):
+            self.biped.set_com_from_waist(self.q0)
+
+            t0 = time()
+            posture = self.biped.solve(CoM, LF, RF, self.q0, e, i)
+            t1 = time()
+            if visualize:
+                v.display(posture)
+                sleep(1)
+
+            pin.forwardKinematics(self.model, self.data, posture)
+            pin.updateFramePlacements(self.model, self.data)
+
+            outLF = self.data.oMf[self.LF_id]
+            outRF = self.data.oMf[self.RF_id]
+
+            self.assertTrue((LF.homogeneous - outLF.homogeneous < e).all())
+            self.assertTrue((RF.homogeneous - outRF.homogeneous < e).all())
+
+            if (pin.centerOfMass(self.model, self.data, posture) - CoM < e).all():
+                break
+
+        self.assertTrue(
+            (pin.centerOfMass(self.model, self.data, posture) - CoM < e).all()
+        )
+        print(
+            (
+                "\nAn error smaller than {} in the"
+                + " CoM was obtained with {} iterations"
+                + "which are computed in {} seconds"
+            ).format(e, i, t1 - t0)
+        )
+        self.assertTrue(i < 20)
 
 
 if __name__ == "__main__":
