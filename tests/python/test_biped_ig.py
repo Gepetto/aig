54b216588c0c3c1e3966f2a87836c1d0b4450306
Nahuel Villa <nvilla@laas.fr>
2022-11-24 09:52:06 +0100

    some arrangement of tests

--- tests/python/test_biped_ig.py
+++ tests/python/test_biped_ig.py
@@ -161,62 +161,6 @@ if __name__ == "__main__":
     unittest.main()
 
     if debug:
-        sleep(5)
-        o = TestBipedIG()
-        o.setUp()
-
-        LF = pin.SE3(np.eye(3), np.array([-0.1, 0.185, 0]))
-        RF = pin.SE3(np.eye(3), np.array([0.0, -0.58, 0]))
-        BS = pin.SE3(np.eye(3), np.array([0, 0, 0.7]))
-
-        # From computation:
-
-        posture = o.biped.solve(BS, LF, RF, o.q0)
-        pin.forwardKinematics(o.model, o.data, posture)
-        pin.updateFramePlacements(o.model, o.data)
-
-        hip_fr_ankle = o.data.oMi[2].translation - o.data.oMi[6].translation
-        print("hip_from_ankle gotten: ", hip_fr_ankle)
-
-        Q7 = np.arctan2(hip_fr_ankle[1], hip_fr_ankle[2])
-
-        print("q7 gotten: ", Q7)
-
-        C_ = np.linalg.norm(hip_fr_ankle[[0, 2]])
-
-        print("C_ gotten: ", C_)
-
-        Q5 = posture[10]
-        C5 = np.cos(Q5)
-        print("C5 gotten: ", C5)
-        print("Q5_ gotten: ", Q5)
-
-        # Repeating step by step
-        # 1 The norm of hip_from_ankle are differnet
-        # 2 The angles q7 are the same.
-        # The error must be on q5, check the computation of c_.
-
-        ee = LF
-        ll_settings = o.biped.get_left_leg_settings()
-
-        hip_ = BS.translation + BS.rotation @ ll_settings.hip_from_waist
-        ankle_ = ee.translation + ee.rotation @ ll_settings.ankle_from_foot
-
-        hip_from_ankle = ee.rotation.T @ (hip_ - ankle_)
-        print("hip_from_ankle should be: ", hip_from_ankle)
-
-        q7 = np.arctan2(hip_from_ankle[1], hip_from_ankle[2])
-
-        print("q7 should be: ", q7)
-
-        c_ = np.linalg.norm(hip_from_ankle[[0, 2]])
-        a_ = np.abs(ll_settings.knee_from_hip[2])
-        b_ = np.abs(ll_settings.ankle_from_knee[2])
-
-        print("c_ should be: ", c_)
-
-        c5 = 0.5 * (c_ * c_ - a_ * a_ - b_ * b_) / (a_ * b_)
-        q5 = np.arccos(c5)
-
-        print("c5 should be: ", c5)
-        print("q5 should be: ", q5)
+        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+        h = TestBipedIG()
+        h.setUp()
